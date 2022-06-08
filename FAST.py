1bf9f1613b65377c7e22fb862b3c8f462b5d6c15
Viviana Osorio <71792220+Vvan31@users.noreply.github.com>
2022-06-08 18:59:41 -0500

    Falta prints

--- FAST.py
+++ FAST.py
@@ -2,7 +2,6 @@ from email.utils import localtime
 import math
 import time
 
-
 global minRTT  
 global sRTT
 global T  
@@ -13,41 +12,33 @@ global bk
 global sst  
 global alpha
 
-
 # Initial global variables 
 minRTT = 20 #ms minimum Round trip time 
-sRTT = 50 #ms   Average Roung 
+sRTT = 50 #ms   Average Round trip time. 
 T = 0.5  # Peso moral de su proyecto, profe. 
 acksRTT = 0 # Numero de paquetes del archivo txt. 
 Tcwnd = 0 # Target Congestion window. 
 cwnd = 0 # Actual Congestion window. 
 bk = 0  # Average flow
 sst = 0 # Slow Start Time.  
-alpha = 0
+alpha = 0 # Parametro de congestion de paqueqtes
 
 # Load data. 
-def loadData():
-    global minRTT  
-    global sRTT
-    global T  
-    global acksRTT 
-    global Tcwnd 
-    global cwnd 
-    global bk 
-    global sst  
-    global alpha
-    acksRTT = 0 # Numero de paquetes del archivo txt
+def loadData(txt): 
+    global acksRTT
+    
+    file = open(txt, 'r')
+    lines = file.readlines()
+    acksRTT = len(lines) # Numero de paquetes del archivo txt
+    file.close()
+    return lines
 
 # Calculate target cwnd
 def calculateTcwnd():
     global minRTT  
-    global sRTT
-    global T  
-    global acksRTT 
+    global sRTT 
     global Tcwnd 
-    global cwnd 
-    global bk 
-    global sst  
+    global cwnd  
     global alpha
     a = (minRTT/sRTT) * cwnd + alpha
     b = -0.5 * cwnd 
@@ -56,29 +47,18 @@ def calculateTcwnd():
 # Calculate Average flow throughput Bk
 
 def calculateBk():
-    global minRTT  
     global sRTT
     global T  
     global acksRTT 
-    global Tcwnd 
-    global cwnd 
     global bk 
-    global sst  
-    global alpha
     bk = T * bk +  (1 - T )*(acksRTT/sRTT) 
     acksRTT = 0
 
 # Calculate ack cwnd 
 def calculateACKcwnd():
-    global minRTT  
-    global sRTT
-    global T  
     global acksRTT 
     global Tcwnd 
     global cwnd 
-    global bk 
-    global sst  
-    global alpha 
     if (Tcwnd > cwnd and acksRTT > 0):
         cwnd = (Tcwnd - cwnd)/acksRTT
     elif(Tcwnd < cwnd):
@@ -86,15 +66,9 @@ def calculateACKcwnd():
 
 # if Loss 
 def calculateIfLoss():
-    global minRTT  
-    global sRTT
-    global T  
-    global acksRTT 
     global Tcwnd 
     global cwnd 
-    global bk 
     global sst  
-    global alpha
     cwnd = cwnd/2
     Tcwnd = cwnd
     sst = cwnd
@@ -114,16 +88,35 @@ def calculateTimeout():
     cwnd = sRTT
     Tcwnd = cwnd
 
-# Calcculate alpha 
+# Calculate alpha 
 def calculateAlpha():
-    alpha = 0           
+    global bk
+    global alpha
+    a1=8
+    a2=20
+    a3=200
+    m0m1=1500
+    m1m0=1250
+    m1m2=15000
+    m2m1=12500
+    bk = int(bk)
 
+    if(alpha == a1 & bk >= m0m1):
+        alpha = a2
+    elif(alpha == a2 & bk <= m1m0):
+        alpha = a1
+    elif(alpha == a2 & bk >= m1m2):
+        alpha = a3
+    elif(alpha == a3 & bk <= m2m1):
+        alpha = a2
+    else:
+        print("gg no alpha: ", alpha)
+    
+    return alpha
 
 def testAck():
     init_time = time.time()
-    file = open("TestTimeout.txt", 'r')
-    lines = file.readlines()
-    sender = lines
+    sender = loadData("TestTimeout.txt")
     receiver = []
     global cwnd
     cwnd = .01
@@ -166,25 +159,47 @@ def TestTimeOut():
     receiver.append(sender[0])
     delta = time.time() - init_time
     cwnd = delta
-'''
 
 # main
 def main(): 
+    
+    global acksRTT 
+    global Tcwnd 
+    global cwnd  
+    global alpha
+    receiver = []
+
     # Enviar paquetes 
+    data_to_send = loadData("TestAck.txt") 
+    
+    for line in  data_to_send:
+        init_time = time.time() #Timestamp
+        
+        receiver.append(line) #Line from one list goes to the other
 
-    # Calcular acksRtt 
+        delta = time.time() - init_time #How much time it took
+        # Calcular BK
+        calculateBk()
+        # Calcular alpha inicial.
+        calculateAlpha()
 
-    # Si acksRtt > a algo Loss 
+        # Si acksRtt > alpha algo Loss 
+        if ( acksRTT > alpha ):
+            calculateIfLoss()
 
-    # if Tcwnd > cwnd calcular cwnd 
+        # if Tcwnd > cwnd calcular cwnd 
+        if (Tcwnd > cwnd):
+            calculateTcwnd()
 
-    # Llamar cada 20ms 
-    calculateBk()
-    # Llamar cada 200 s
-    calculateAlpha()
+        # Llamar cada 20ms 
+        if ((int(delta*10))%2 == 0):
+            calculateTcwnd()
 
-'''            
+        # Llamar cada 200 s
+        if (int(delta/100)%2 == 0):
+            calculateAlpha()
+        print("Parameters: \n\tTcwnd: ", Tcwnd, "\n\tCwnd: ", cwnd)
 
 
     
-testLoss()
+main()
